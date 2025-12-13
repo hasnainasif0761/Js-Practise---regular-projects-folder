df58acfbfc3eb8dd8599e3c0c1b72351399f346a
hasnainasif0761 <hasnainasif07610761@gmail.com>
2025-12-13 21:25:48 +0500

    'First-Change'

---
+++ 12-12-2025/script.js
@@ -0,0 +1,49 @@
+// var number = 4.55;
+// var roundedNumber = number.toFixed(2);
+// console.log("The rounded number is " + roundedNumber);
+
+
+// var num = 5.55;
+// var rounded = num.toString();
+// if(rounded.charAt(rounded.length - 1) == '5'){
+//    rounded = rounded.slice(0, rounded.length-1) + '6'; 
+//    console.log('The Answere of num is',rounded);
+// }
+
+// Unix epoch time
+// var date = {
+//     getfullYear : "2025",
+//     getMonth : 11
+// }
+
+// console.log('The Year and month is =>',date.getfullYear + "-" + date.getMonth);
+
+
+// var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+// var Dated = new Date();
+// console.log(days[Dated.getDay()]);
+
+
+// var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+// var month = new Date();
+// console.log('The Current month is=>',months[month.getMonth()]);
+
+
+
+// var mydate = new Date("january 35, 2023");  // ya invalid date is leya hute ha kio ka January month me 35 days nahi hote 
+// console.log('Your Date is =>',mydate);
+
+
+var mydate = new Date(2030, 0, 30); 
+console.log(mydate);
+
+var currentDate = new Date();
+console.log('The Current Date is =>',currentDate);
+
+var diffrence = mydate - currentDate;
+console.log('The Diffrence is =>',diffrence);
+
+
+
