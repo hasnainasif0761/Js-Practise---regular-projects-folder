922278d9cea6ac50931793fd68825b78a5f7f98b
hasnainasif0761 <hasnainasif07610761@gmail.com>
2025-12-18 11:56:39 +0500

    '16-12-2025'

---
+++ 16-12-2025/script.js
@@ -0,0 +1,12 @@
+// Read Only
+// alert('Hello');
+
+
+// Warning show for this console
+// console.warn('Hello Baraha karam tankar sa doree raha ')
+
+// Error Show for this console
+// console.error("Num is not undefined")
+
+
+
