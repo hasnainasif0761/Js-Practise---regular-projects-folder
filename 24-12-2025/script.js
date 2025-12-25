dbc969860bda74434b716ba1cdfef239128a058b
hasnainasif0761 <hasnainasif07610761@gmail.com>
2025-12-25 13:39:22 +0500

    'New_Change_for_Update_Class_12-25-2025'

--- 24-12-2025/script.js
+++ 24-12-2025/script.js
@@ -1,27 +1,34 @@
-// // alert('Hello , Welcome to Javascript Class Revesion');
+// // // alert('Hello , Welcome to Javascript Class Revesion');
 
-// let arr = ['Saad','Bilal','Asad'];
-// let uname = prompt('Enter your Absent Student Name');
+// // let arr = ['Saad','Bilal','Asad'];
+// // let uname = prompt('Enter your Absent Student Name');
 
-// let unametofirstChar = uname.slice(0,1)
-// let unameFirstCharUpperCase = unametofirstChar.toUpperCase();
-// let unameLastChar = uname.slice(1);
-// let unameLastChartoLowerCase = unameLastChar.toLocaleLowerCase();
+// // let unametofirstChar = uname.slice(0,1)
+// // let unameFirstCharUpperCase = unametofirstChar.toUpperCase();
+// // let unameLastChar = uname.slice(1);
+// // let unameLastChartoLowerCase = unameLastChar.toLocaleLowerCase();
 
-// let fullname = unameFirstCharUpperCase + unameLastChartoLowerCase;
-
-
-// for(let i = 0; i < arr.length;i++){
-//     if(fullname == arr[i]){
-//         console.log('The',fullname,'is Absent');
-//         break;
-//     }else
-//     {
-//         console.log('You are present',arr[i]);
-//     }
-// }
+// // let fullname = unameFirstCharUpperCase + unameLastChartoLowerCase;
 
 
+// // for(let i = 0; i < arr.length;i++){
+// //     if(fullname == arr[i]){
+// //         console.log('The',fullname,'is Absent');
+// //         break;
+// //     }else
+// //     {
+// //         console.log('You are present',arr[i]);
+// //     }
+// // }
+
+// var num = prompt('Enter a Number age');
+
+// var numberConvert = parseInt(num) + 10;
+// var FloatConvert = parseFloat(num) + 10;
+
+// console.log(typeof numberConvert);
+// console.log(numberConvert);
+// console.log(FloatConvert);
 
 
 
