a158c7e2563e9b4232afe2e7d1b6b864feacb876
hasnainasif0761 <hasnainasif07610761@gmail.com>
2025-12-25 13:31:37 +0500

    'Second-Change'

---
+++ 24-12-2025/script.js
@@ -0,0 +1,35 @@
+// // alert('Hello , Welcome to Javascript Class Revesion');
+
+// let arr = ['Saad','Bilal','Asad'];
+// let uname = prompt('Enter your Absent Student Name');
+
+// let unametofirstChar = uname.slice(0,1)
+// let unameFirstCharUpperCase = unametofirstChar.toUpperCase();
+// let unameLastChar = uname.slice(1);
+// let unameLastChartoLowerCase = unameLastChar.toLocaleLowerCase();
+
+// let fullname = unameFirstCharUpperCase + unameLastChartoLowerCase;
+
+
+// for(let i = 0; i < arr.length;i++){
+//     if(fullname == arr[i]){
+//         console.log('The',fullname,'is Absent');
+//         break;
+//     }else
+//     {
+//         console.log('You are present',arr[i]);
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
