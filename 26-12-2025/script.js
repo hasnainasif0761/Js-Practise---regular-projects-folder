576e9b90e0753e2922da2aa70b083d98281adf10
hasnainasif0761 <hasnainasif07610761@gmail.com>
2025-12-27 12:20:37 +0500

    'Fourth-Change'

---
+++ 26-12-2025/script.js
@@ -0,0 +1,32 @@
+// /*
+// var age = +prompt("Enter your age:");
+
+// switch (age) {
+//     case 10:
+//         console.log("Your age is =>", age);
+//         break;
+//     case 20:
+//         console.log('Your age is', age);
+//         break;
+//     case 30:
+//         console.log('Your age is', age);
+//         break;
+//     default:
+//         console.log('Your are Hacker');
+// }
+//         */
+
+// // var i = 1;
+
+// // while(i < 6){
+// //     console.log('Your age is =>', i);
+// //     i = i +1;
+// // }
+// // console.log(i);
+
+// var age = +prompt("Enter your age:");
+
+// do{
+//     console.log('Your age is =>',age);
+// }
+// while(age < 0);
